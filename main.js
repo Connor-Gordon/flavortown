3d8634635ebd584a96228bc4ddcd6af4023d4e16
Connor Gordon <con.gordon@gmail.com>
2018-12-13 16:33:54 -0800

    initial commit for branch

---
+++ main.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+// tab section
+	$('#tab2, #tab3').hide()
+	$('.tabList li:nth-child(1) a').addClass('active')
+	$('.tabList, .tabContainer').css('visibility', 'visible')
+	$('.tabList').on('click', 'li a', function(e){
+		e.preventDefault()
+		var href=$(this).attr('href')
+		$('.tabContainer div').hide()
+		$(href).fadeIn()
+		$('.tabList li a').revomeClass('active')
+		$(this).addClass('active')
+	})
+
+	
+
+
+
+})
+
+
