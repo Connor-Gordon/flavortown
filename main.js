75b7f694eaad3142952140d836799e482d739c14
Connor Gordon <con.gordon@gmail.com>
2018-12-16 07:13:13 -0800

    fixed css from merge

--- main.js
+++ main.js
@@ -15,49 +15,68 @@ $(document).ready(function(){
 		$(this).addClass('active')
 	})
 
+// todays special
+	// let specialTemplate
+	
+ //    $.get('https://obscure-tundra-54269.herokuapp.com/bar-food', function(data){
+ //        specialTemplate = `
+ //        <ul>
+ //        	<li>${data.entrees[0].name}</li>
+ //        	<li>${data.entrees[0].description}</li>
+ //        </ul>`
+
+ //        let specialContent = document.getElementById('#food-desc')
+ //        specialContent.innerHTML += specialTemplate
+ //        console.log(specialTemplate)
+        
+ //   })
 
 // menu section from api
 	
-	var appTemplate = ''
-    $.get('https://obscure-tundra-54269.herokuapp.com/bar-food', function(data){
-        data.appetizers.forEach(function(item){
-            appTemplate += `
-                <ul id="appetizertext">
-                    <li id="appetizername">${item.name}</li>
-                    <li id="appetizerprice">${item.price}</li>
-                    <li id="appetizerdesc">${item.description}</li>
-                    <li id="appetizerextra">intentionally blank</li>
-                </ul>
-            `
-        })
-        var appContent = document.getElementById('menu')
-        appContent.innerHTML = appTemplate
-        console.log(appTemplate)
-    })
+	// let appTemplate = ""
+ //    $.get('https://obscure-tundra-54269.herokuapp.com/bar-food', function(data){
+ //        data.appetizers.forEach(function(item){
+ //            appTemplate = `
+ //                <ul id="appetizertext">
+ //                    <li id="appetizername">${item.name}</li>
+ //                    <li id="appetizerprice">$${item.price}</li>
+ //                    <li id="appetizerdesc">${item.description}</li>
+ //                    <li id="appetizerextra">${item.extra}</li>
+ //                </ul>
+ //            `
+ //        })
+ //        let appContent = document.getElementById('menu')
+ //        appContent.innerHTML = appTemplate
+ //        console.log(appTemplate)
+ //    })
 
+// let isSpicy = ""
 
+// if(item.extra.spicy){
+// 	return isSpicy
+// }
 
 
 // menu section from menu.js
 	
-	// var searchTemplate = ""
+	var searchTemplate = ""
 
-	// menu.appetizers.forEach(function(menuItem){
+	menu.appetizers.forEach(function(menuItem){
 		
-	// 	searchTemplate += 
-	// 	`<div id="menuDiv">
-	// 		<ul class="templateList">
-	// 			<li class="templateName"> ${menuItem.name}</li>
-	// 			<li class="templateDescription"> ${menuItem.description}</li>
-	// 			<li class="templatePrice"> ${menuItem.price}</li>
-	// 			<li class="templateExtra"> </li>
-	// 		</ul>		
-	// 	</div>`
+		searchTemplate += 
+		`<div id="menuDiv">
+			<ul class="templateList">
+				<li class="templateName"> ${menuItem.name}</li>
+				<li class="templateDescription"> ${menuItem.description}</li>
+				<li class="templatePrice"> ${menuItem.price}</li>
+				<li class="templateExtra"> </li>
+			</ul>		
+		</div>`
 
-	// 		var elements = document.getElementById("menu")
-	// 		elements.innerHTML = searchTemplate
+			var elements = document.getElementById("menu")
+			elements.innerHTML = searchTemplate
 	
-	// })
+	})
 
 	
 
